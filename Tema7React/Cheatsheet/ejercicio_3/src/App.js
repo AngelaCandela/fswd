fddd4e82b4873c78e956151a5926ebbc6ebd7856
AngelaCandela <angelacandela@hotmail.it>
2020-12-22 14:15:20 +0100

    Continuamos Ejercicio 3(antes llamado Ejercicio 4) de React

---
+++ Tema7React/Cheatsheet/ejercicio_3/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { useState } from "react";
+import Agenda from "./components/Agenda";
+import Formulario from "./components/Formulario";
+
+function App() {
+
+  const [contacts, setContacts] = useState([
+    {name: "John", lastName: "Smith", address: "Calle Compositor", province: "Almería", postcode: "04008", phone: "648560349"},
+    {name: "Sarah", lastName: "Donovan", address: "Calle Marqués de Larios", province: "Málaga", postcode: "29008", phone: "661284186"},
+    {name: "Kate", lastName: "Doe", address: "Calle 2", province: "Madrid", postcode: "29630", phone: "611284133"},
+  ]);
+
+  return (
+    <div className="container">
+      <Agenda contacts={contacts}/>
+      <Formulario setContacts={setContacts}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Tema7React/Cheatsheet/ejercicio_3/src/components/Formulario.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function Formulario({ setContacts }) {
+
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [address, setAddress] = useState("");
+    const [province, setProvince] = useState("");
+    const [postcode, setPostcode] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const handleName= (event) => {
+        setName(event.target.value);
+    }
+
+    const handleLastName= (event) => {
+        setLastName(event.target.value);
+    }
+
+    const handleAddress= (event) => {
+        setAddress(event.target.value);
+    }
+
+    const handleProvince= (event) => {
+        setProvince(event.target.value);
+    }
+
+    const handlePostcode= (event) => {
+        setPostcode(event.target.value);
+    }
+
+    const handlePhone= (event) => {
+        setPhone(event.target.value);
+    }
+
+    return (
+        <form className="form-group">
+            <h1>New contact</h1>
+            <input type="text" className="form-control mb-3" placeholder="Name" value={name} onChange={handleName}></input>
+            <input type="text" className="form-control mb-3" placeholder="Last Name" value={lastName} onChange={handleLastName}></input> 
+            <input type="text" className="form-control mb-3" placeholder="Address" value={address} onChange={handleAddress}></input> 
+            <input type="text" className="form-control mb-3" placeholder="Province" value={province} onChange={handleProvince}></input> 
+            <input type="text" className="form-control mb-3" placeholder="Postcode" value={postcode} onChange={handlePostcode}></input> 
+            <input type="text" className="form-control mb-3" placeholder="Phone" value={phone} onChange={handlePhone}></input> 
+            <input type="submit" value="Add contact" className="btn btn-success"></input>         
+        </form>
+    )
+}
